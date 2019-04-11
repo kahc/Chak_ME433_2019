b211ffd493df4a53ca303a36627e715d76c6a0ab
Chak Lee <chaklee000@gmail.com>
2019-04-11 15:38:43 -0500

    got 1kHz square wave working

--- HW1/main.c
+++ HW1/main.c
@@ -5,28 +5,28 @@
 #pragma config DEBUG = 0b11 // no debugging
 #pragma config JTAGEN = 0 // no jtag
 #pragma config ICESEL = 0b11 // use PGED1 and PGEC1
-#pragma config PWP = 0x1FF // no write protect
+#pragma config PWP = 0b111111 // no write protect
 #pragma config BWP = 1 // no boot write protect
 #pragma config CP = 1 // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 0x011 // use primary oscillator with pll
+#pragma config FNOSC = 0b011 // use primary oscillator with pll
 #pragma config FSOSCEN = 0 // turn off secondary oscillator
 #pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 0 // high speed crystal mode
+#pragma config POSCMOD = 0b01 // high speed crystal mode
 #pragma config OSCIOFNC = 1 // disable secondary osc
 #pragma config FPBDIV = 0 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = 0x11 // do not enable clock switch
-#pragma config WDTPS = 0x10100 // use slowest wdt
+#pragma config FCKSM = 0b11 // do not enable clock switch
+#pragma config WDTPS = 0b10100 // use slowest wdt
 #pragma config WINDIS = 1 // wdt no window mode
 #pragma config FWDTEN = 0 // wdt disabled
 #pragma config FWDTWINSZ = 0b11 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = 0x001 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = 0x111 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = 0x001 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = 0x001 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config FPLLIDIV = 0b001 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = 0b111 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = 0b001 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = 0b001 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
 #pragma config UPLLEN = 0 // USB clock on
 
 // DEVCFG3
@@ -63,7 +63,7 @@ int main() {
     while(1) {
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
-        if(_CP0_GET_COUNT() > 18000){
+        if(_CP0_GET_COUNT() > 24000){
             _CP0_SET_COUNT(0);
             LATAbits.LATA0 = !LATAbits.LATA0;
         }
