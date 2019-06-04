b54f98637f6ee60566de08bd2c48636dfe5d1554
Chak Lee <chaklee000@gmail.com>
2019-06-04 16:37:35 -0500

    created HW5 folder
    also changed config file to default analog input to off

---
+++ HW5.X/PIC_config.c
@@ -0,0 +1,31 @@
+#include "PIC_config.h"
+
+void board_setup(){
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // setup user_LED
+    TRISAbits.TRISA0 = 0;
+    
+    //setup user_button
+    TRISAbits.TRISA1 = 1;
+    ANSELAbits.ANSA1 = 0;
+    user_LED = false;
+    
+    // turning off all analog inputs because the default is silly
+    ANSELA = 0;
+    ANSELB = 0;
+    
+    __builtin_enable_interrupts();
+}
---
+++ HW5.X/main.c
@@ -0,0 +1,9 @@
+#include "PIC_config.h"
+#include "config_bits.h"
+
+int main(){
+    board_setup();
+
+    
+    
+}
