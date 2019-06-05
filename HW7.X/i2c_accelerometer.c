3a15b99fbfa59e9448dda7746444d333ffa3d12d
Chak Lee <chaklee000@gmail.com>
2019-06-05 18:27:34 -0500

    HW7 files organizaed
    also added video to HW5 & 6

---
+++ HW7.X/i2c_accelerometer.c
@@ -0,0 +1,3 @@
+#include "i2c_accelerometer.h"
+#include "i2c_master_noint.h"
+
--- HW7.X/main.c
+++ HW7.X/main.c
@@ -7,14 +7,22 @@ int main(){
     board_setup();
     SPI1_init();
     LCD_init();
-    LCD_clearScreen(ILI9341_GREEN);
-    LCD_drawRectangle(10,10, 200, 200, ILI9341_PURPLE);
     
+    LCD_clearScreen(ILI9341_GREEN);
+    LCD_drawRectangle(30, 150, 50, 175, ILI9341_PURPLE);
+    
+    int heartbeat_count = 0;
     
     while(true){
         // 10Hz cycles
         if(_CP0_GET_COUNT() > 2400000){
             _CP0_SET_COUNT(0);
+            
+            heartbeat_count++;
+            if(heartbeat_count > 500){
+                heartbeat_count = 0;
+                user_LED = !user_LED;
+            }
         }
     }
     
