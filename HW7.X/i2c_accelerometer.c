284c87ef51462a46c36b1ae70b3d14cf855c57c4
Chak Lee <chaklee000@gmail.com>
2019-06-05 21:36:25 -0500

    read/write problems
    copied over from HW5

--- HW7.X/i2c_accelerometer.c
+++ HW7.X/i2c_accelerometer.c
@@ -1,3 +1,34 @@
 #include "i2c_accelerometer.h"
 #include "i2c_master_noint.h"
 
+// define addresses
+#define accelerometer_address 0b0100000      // expander i2c address
+
+void init_accelerometer(){
+    i2c_master_setup();
+}
+
+void write_accelerometer(unsigned char reg, char value){
+    i2c_master_start();
+    i2c_master_send(accelerometer_address << 1 |0);
+    i2c_master_send(reg);
+    i2c_master_send(value);
+    i2c_master_stop();
+}
+unsigned char read_accelerometer(unsigned char reg){
+    // move to correct register
+    i2c_master_start();
+    i2c_master_send(accelerometer_address << 1 | 0);
+    i2c_master_send(reg);
+    i2c_master_restart();
+    
+    // read into local copy
+    i2c_master_send(reg << 1 | 1);
+    char temp = i2c_master_recv();
+    i2c_master_ack(1);
+    i2c_master_stop();
+    return temp;
+}
+void I2C_read_multiple(unsigned char address, unsigned char reg, unsigned char * data, int length){
+    
+}
--- HW7.X/i2c_accelerometer.h
+++ HW7.X/i2c_accelerometer.h
@@ -3,15 +3,10 @@
 #ifndef I2C_PIN_ACCEL_H    /* Guard against multiple inclusion */
 #define I2C_PIN_ACCEL_H
 
-// define addresses
-#define expander_gpio 0x09      // expander port/gpio register address
-#define expander_iodir 0x00     // expander io direction register address
-#define expander_address 0b0100000      // expander i2c address
-
 // function declarations
 void init_accelerometer();
 void write_accelerometer(unsigned char reg, char level);
-unsigned char read_accelerometer();
+unsigned char read_accelerometer(unsigned char reg);
 void I2C_read_multiple(unsigned char address, unsigned char reg, unsigned char * data, int length);
 
 #endif
