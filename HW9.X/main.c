062ea43e49c892f1bb80ee3f48339c79c9e33440
Chak Lee <chaklee000@gmail.com>
2019-06-09 12:31:40 -0500

    map touchscreen to coordinates

--- HW9.X/main.c
+++ HW9.X/main.c
@@ -12,6 +12,7 @@ int main(){
     
     LCD_clearScreen(ILI9341_GREEN); 
     
+    unsigned short touch_x_raw, touch_y_raw;
     unsigned short touch_x, touch_y;
     unsigned int touch_z;
     char message[40];
@@ -23,9 +24,13 @@ int main(){
         if(_CP0_GET_COUNT() > 1200000){
             _CP0_SET_COUNT(0);
             
-            touchscreen_read(&touch_x, &touch_y, &touch_z);
+            touchscreen_read(&touch_x_raw, &touch_y_raw, &touch_z);
             
-            sprintf(message, "touch_x: %d, touch_y: %d, touch_z: %d", touch_x, touch_y, touch_z);
+            touch_x = (touch_x_raw - 400) * 240.0/3500.0;
+            touch_y = -(touch_y_raw - 3900) * 360.0/3500.0;
+            
+            
+            sprintf(message, "touch_x: %d, touch_y: %d, touch_z: %d        ", touch_x, touch_y, touch_z);
             LCD_drawWord(5, 50, ILI9341_RED, message);
             
             heartbeat_count++;
